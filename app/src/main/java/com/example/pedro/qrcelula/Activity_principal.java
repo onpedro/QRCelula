4bc254ef62b2838955a366d5b0252774cd0f683c
pedro <pedroarthurpassos@gmail.com>
2017-09-03 11:08:23 -0300

    Commit final, versão completa.

--- app/src/main/java/com/example/pedro/qrcelula/Activity_principal.java
+++ app/src/main/java/com/example/pedro/qrcelula/Activity_principal.java
@@ -3,11 +3,18 @@ package com.example.pedro.qrcelula;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.media.session.MediaController;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -16,12 +23,17 @@ public class Activity_principal extends AppCompatActivity {
 private Button btnSobre;
 private Button btnAcesso;
     private MediaPlayer m;
+    VideoView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        final Activity activity = Activity_principal.this;
+        view = (VideoView)findViewById(R.id.videoView2);
+        String path = "android.resource://" +getPackageName()+"/"+R.raw.videointro;
+        view.setVideoURI(Uri.parse(path));
+        view.seekTo(0);
+        view.start();
 
         btnSobre = (Button) findViewById(R.id.btnNos);
         btnAcesso = (Button) findViewById(R.id.btnAcessar);
@@ -36,7 +48,7 @@ private Button btnAcesso;
         btnAcesso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                IntentIntegrator integrator = new IntentIntegrator(activity);
+                IntentIntegrator integrator = new IntentIntegrator(Activity_principal.this);
                 integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
                 integrator.setPrompt("Bem vindo ao scanner!");
                 integrator.setCameraId(0);
